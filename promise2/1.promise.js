32b3f409c274757d433be6ae2ba652baea4a15d0
lyf <lin13177896353@163.com>
2021-10-10 12:41:25 +0800

    promise

--- promise2/1.promise.js
+++ promise2/1.promise.js
@@ -15,16 +15,100 @@
 // executor 默认就会执行
 // 默认创建一个promise 状态就是pending
 // 每一个promise实例都有then
-const MyPromise = require('./mypromise.js')
+// const MyPromise = require('./mypromise.js')
 
-let p = new MyPromise((resolve, reject) => {
-  // throw new Error('1111')
-  resolve('123') // fulfilled
-  // reject('123') //rejected
+// let p = new MyPromise((resolve, reject) => {
+//   // throw new Error('1111')
+//   setTimeout(() => {
+//     resolve('123') // fulfilled
+//   }, 1000)
+//   // reject('123') //rejected
+// })
+// p.then((res) => {
+//   console.log(res)
+// }, (err) => {
+//   console.log(err)
+// })
+// console.log(1)
+// promise的链式调用问题
+// 1）如果then 方法中（成功或失败）返回的不是一个promise 会将这个值传递给外层下一次then的成功结果
+
+// const fs = require('fs')
+// function read (...args) {
+//   return new Promise((resolve, reject) => {
+//     fs.readFile(...args, function (err, data) {
+//       if (err) reject(err)
+//       resolve(data)
+//     })
+//   })
+// }
+// read('promise2/age.txt', 'utf8').then(
+//   data => {
+//     return 1000
+//   },
+//   err => {
+//     console.log(err)
+//   }
+// ).then(data => {
+//   console.log(data) ///10000
+// }, err => {
+//   console.log('err', err)
+// })
+
+// 2）如果执行then方法中的方法出错抛出异常 会走到当前的失败中
+
+// const fs = require('fs')
+// function read (...args) {
+//   return new Promise((resolve, reject) => {
+//     fs.readFile(...args, function (err, data) {
+//       if (err) reject(err)
+//       resolve(data)
+//     })
+//   })
+// }
+// read('promise2/age.txt', 'utf8').then(
+//   data => {
+//     throw new Error('222')
+//   },
+//   err => {
+//     console.log(err)
+//   }
+// ).then(data => {
+//   console.log(data)
+// }, err => {
+//   console.log('err', err)
+// })
+
+// 如果返回的是一个promise 会用这个promise的结果作为下一次then的成功或者失败
+const fs = require('fs')
+const MyPromise = require('./mypromise')
+function read (...args) {
+  return new MyPromise((resolve, reject) => {
+    fs.readFile(...args, function (err, data) {
+      if (err) reject(err)
+      resolve(data)
+    })
+  })
+}
+read('promise2/age.txt', 'utf8').then(
+  data => {
+    return new MyPromise((resolve, reject) => {
+      resolve(1111111)
+    })
+  },
+  err => {
+    console.log(err)
+  }
+).then(data => {
+  console.log('2', data)
+}, err => {
+  console.log('err', err)
+}).then(data => {
+  console.log('3', data)
+}, err => {
+  console.log('err', err)
 })
-p.then((res) => {
-  console.log(res)
-}, (err) => {
-  console.log(err)
-})
-console.log(1)
+
+// 1 出错会失败 返回的promise会失败
+// then  为什么可以链式调用 每次都会返回一个新的promise
+
--- promise2/mypromise.js
+++ promise2/mypromise.js
@@ -3,21 +3,57 @@ const STATUS = {
   FULFIllED: 'FULFIllED',
   REJECTED: 'REJECTED'
 }
+function resolvePromise (x, promise2, resolve, reject) {
+  if (x === promise2) { //防止自己等待自己
+    return reject(new Error('出错了'))
+  } else if (Object.prototype.toString.call(x) == '[object Function]' || typeof x == 'object' && x !== null) {
+    let called
+    try {
+      // 如果是then 是一个函数 我就认为他是个函数
+      if (Object.prototype.toString.call(x.then) == '[object Function]') {
+        x.then.call(x, data => {
+          if (called) return
+          called = true
+          // data 可能还是一个promise
+          resolvePromise(data, promise2, resolve, reject)
+        }, err => {
+          if (called) return
+          called = true
+          reject(err)
+        })
+      } else {
+        if (called) return
+        called = true
+        resolve(x)
+      }
+    } catch (error) {
+      if (called) return
+      called = true
+      reject(error)
+    }
+  } else {
+    resolve(x)
+  }
+}
 class MyPromise {
   constructor(executor) {
     this.status = STATUS.PENDING
     this.val = undefined
     this.reason = undefined
+    this.onResolveCallbacks = []
+    this.onRejectCallbacks = []
     const resolve = (val) => {
       if (this.status == STATUS.PENDING) {
         this.status = STATUS.FULFIllED
         this.val = val
+        this.onResolveCallbacks.forEach(fn => fn())
       }
     }
     const reject = (reason) => {
       if (this.status == STATUS.PENDING) {
         this.status = STATUS.REJECTED
         this.reason = reason
+        this.onRejectCallbacks.forEach(fn => fn())
       }
     }
     try {
@@ -27,12 +63,47 @@ class MyPromise {
     }
   }
   then (onfulfilled, onRejected) {
-    if (this.status == STATUS.FULFIllED) {
-      onfulfilled(this.val)
-    }
-    if (this.status == STATUS.REJECTED) {
-      onRejected(this.reason)
-    }
+    let promise2 = new MyPromise((resolve, reject) => {
+      if (this.status == STATUS.FULFIllED) {
+        setTimeout(() => {
+          try {
+            resolvePromise(onfulfilled(this.val), promise2, resolve, reject)
+          } catch (error) {
+            reject(error)
+          }
+        }, 0)
+      }
+      if (this.status == STATUS.REJECTED) {
+        setTimeout(() => {
+          try {
+            resolvePromise(onRejected(this.reason), promise2, resolve, reject)
+          } catch (error) {
+            reject(error)
+          }
+        }, 0)
+      }
+      if (this.status == STATUS.PENDING) {
+        this.onResolveCallbacks.push(() => {
+          setTimeout(() => {
+            try {
+              resolvePromise(onfulfilled(this.val), promise2, resolve, reject)
+            } catch (error) {
+              reject(error)
+            }
+          }, 0)
+        })
+        this.onRejectCallbacks.push(() => {
+          setTimeout(() => {
+            try {
+              resolvePromise(onRejected(this.reason), promise2, resolve, reject)
+            } catch (error) {
+              reject(error)
+            }
+          }, 0)
+        })
+      }
+    })
+    return promise2
   }
 }
 module.exports = MyPromise
