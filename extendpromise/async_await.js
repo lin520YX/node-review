17a700a46789d71aa8ebafea8bd892e57ddfe018
lyf <lin13177896353@163.com>
2021-10-12 23:01:16 +0800

    async await

---
+++ extendpromise/async_await.js
@@ -0,0 +1,8 @@
+// async function read () { // switch-case => babel 编译之后就是把一个函数分成多个case 采用指针的方式向下移动
+//   let a = await b()
+//   let b = await c()
+//   return b
+// }
+
+// async 方法执行之后返回的是一个promise 
+// async + await 是generator 的语法糖
--- extendpromise/generator.js
+++ extendpromise/generator.js
@@ -21,44 +21,83 @@
 
 
 
-const context = {
-  prev: 0, //当前 
-  next: 0, //下一次
-  done: false,
-  stop () {
-    this.done = true //更改完成状态
-  }
+// const context = {
+//   prev: 0, //当前 
+//   next: 0, //下一次
+//   done: false,
+//   stop () {
+//     this.done = true //更改完成状态
+//   }
+// }
+
+// function gen$ (context) {
+//   switch (context.prev = context.next) {
+//     case 0:
+//       context.next = 1;
+//       return 1
+//     case 1:
+//       context.next = 2;
+//       return 2
+//     case 2:
+//       context.next = 3;
+//       return 3
+//     case 3:
+//       context.stop;
+//       return 100
+//   }
+// }
+
+// let gen = function () {
+//   return {
+//     next () {
+//       //传入上下文
+//       return {
+//         value: gen$(context),
+//         done: context.done
+//       }
+//     }
+//   }
+// }
+// let it = gen()
+// console.log(it.next())
+// console.log(it.next())
+// console.log(it.next())
+
+
+
+function* read () {
+  let name = yield new Promise((resolve, reject) => {
+    resolve(1)
+  })
+  let age = yield new Promise((resolve, reject) => {
+    resolve(2222)
+  })
+  return age
 }
 
-function gen$ () {
-  switch (context.prev = context.next) {
-    case 0:
-      context.next = 1;
-      return 1
-    case 1:
-      context.next = 2;
-      return 2
-    case 2:
-      context.next = 3;
-      return 3
-    case 3:
-      context.stop;
-      return 100
-  }
-}
+// let it = read()
+// it.next().value.then(res => {
+//   let { value } = it.next()
+//   value(res).then(data => {
+//     console.log(data)
+//   })
+// })
 
-let gen = function () {
-  return {
-    next () {
-      //传入上下文
-      return {
-        value: gen$(context),
-        done: context.done
+function co (it) {
+  return new Promise((resolve, reject) => {
+    function next (data) {
+      let { value, done } = it.next(data)
+      if (!done) {
+        Promise.resolve(value).then(res => {
+          next(res)
+        }, reject)
+      } else {
+        resolve(value)
       }
     }
-  }
+    next()
+  })
 }
-let it = gen()
-console.log(it.next())
-console.log(it.next())
-console.log(it.next())
+co(read()).then(res => {
+  console.log('co', res)
+})
