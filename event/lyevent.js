78739fa46d8587413b27d5f93f7ab02399556058
lyf <lin13177896353@163.com>
2021-10-18 00:49:04 +0800

    lyfevent

--- event/lyevent.js
+++ event/lyevent.js
@@ -1,10 +1,38 @@
 function EventEmitter () {
+  this._events = {}
 
 }
-EventEmitter.prototype.on = function () {
-
+EventEmitter.prototype.on = function (eventName, callback) {
+  if (!this._events) {
+    this._events = Object.create(null)
+  }
+  if (eventName != 'newListener') {
+    this.emit('newListener', eventName)
+  }
+  if (this._events[eventName]) {
+    this._events[eventName].push(callback)
+  } else {
+    this._events[eventName] = [callback]
+  }
 }
-EventEmitter.prototype.emit = function () {
-
+EventEmitter.prototype.emit = function (eventName, ...args) {
+  if (!this._events) return
+  if (this._events[eventName]) {
+    this._events[eventName].forEach(fn => {
+      fn(...args)
+    });
+  }
 }
-module.exports = EventEmitter
+EventEmitter.prototype.once = function (eventName, callback) {
+  const once = (...args) => {
+    callback(...args)
+    this.off(eventName, once)
+  }
+  once.l = callback
+  this.on(eventName, once)
+}
+EventEmitter.prototype.off = function (eventName, callback) {
+  if (!this._events) return
+  this._events[eventName] = this._events[eventName].filter(fn => ((fn !== callback) && (fn.l != callback)))
+}
+// module.exports = EventEmitter
