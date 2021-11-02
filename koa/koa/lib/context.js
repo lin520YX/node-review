0fd75376904c32626424072539885f09ff152ad2
lyf <lin13177896353@163.com>
2021-11-02 23:09:10 +0800

    con

--- koa/koa/lib/context.js
+++ koa/koa/lib/context.js
@@ -1,1 +1,12 @@
-module.exports = {}
+let proto = {
+
+}
+module.exports = proto
+
+function defineGetter (target, key) {
+  proto.__defineGetter__(key, function () {
+    return this[target][key]
+  })
+}
+
+defineGetter('request', 'path')
--- koa/koa/lib/request.js
+++ koa/koa/lib/request.js
@@ -1,7 +1,12 @@
 const url = require('url')
 module.exports = {
-  get path(){
-    let {pathname} = url.parse(this.req.url)
+  get path () {
+    let { pathname } = url.parse(this.req.url)
     return pathname
+  },
+  get query () {
+    let { query } = url.parse(this.req.url, true)
+    return query
   }
 }
+// console.log(url.parse('http://aaaa.com?aaa=vvv', true).query.aaa)
