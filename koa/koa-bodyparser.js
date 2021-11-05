58c15db56381030661280176d66dc41d6efbc5aa
lyf <lin13177896353@163.com>
2021-11-06 00:31:43 +0800

    解析

--- koa/koa-bodyparser.js
+++ koa/koa-bodyparser.js
@@ -6,6 +6,18 @@ module.exports = function bodyParser () {
   }
 }
 
+Buffer.prototype.split = function (bondary) {
+  let arr = []
+  let offset = 0
+  let currentPosition = 0
+  while (-1 != (currentPosition = this.indexOf(bondary, offset))) {
+    arr.push(this.slice(offset, currentPosition))
+    offset = currentPosition + bondary.length
+  }
+  arr.push(this.slice(offset))
+  return arr
+}
+
 async function body (ctx) {
   return new Promise((resolve, reject) => {
     let arr = []
@@ -22,8 +34,47 @@ async function body (ctx) {
       // Content-Type: application/x-ww-form-urlencoded
       let data = Buffer.concat(arr)
       let type = ctx.get('Content-Type')
+      // console.log(type)
       if (type == 'application/x-www-form-urlencoded') { //a=1&b=2
         resolve(querystring.parse(data.toString()))
+      } else if (type == 'application/json') {
+        resolve(JSON.parse(data.toString()))
+      } else if (type == 'text/plain;charset=UTF-8') {
+        resolve(data.toString())
+      } else if (type.startsWith('multipart/form-data')) {
+        // ------WebKitFormBoundary8AazAfGT7jwg3TEJ
+        // Content-Disposition: form-data; name="username"
+
+        // 12321
+        // ------WebKitFormBoundary8AazAfGT7jwg3TEJ
+        // Content-Disposition: form-data; name="password"
+
+        // 312321
+        // ------WebKitFormBoundary8AazAfGT7jwg3TEJ
+        // Content-Disposition: form-data; name="avatar"; filename="node.txt"
+        // Content-Type: text/plain
+
+        // dsfsdfdsfs
+
+        // ------WebKitFormBoundary8AazAfGT7jwg3TEJ--
+        // 分段传输 给我当前客户端提交的数据和一个分隔符 multipart/form-data 后面会给个bondary
+        // console.log(data.toString())
+
+        let bondary = '--' + type.split('=')[1]
+        let lines = data.split(bondary)
+        lines = lines.slice(1, -1)
+        let resultObj = {}
+        lines.forEach(line => {
+          let [head, body] = line.split('\r\n\r\n')
+          if (head) {
+            let key = head.toString().match(/name="(.+?)"/)[1]
+            if (!head.includes('filename')) {
+              resultObj[key] = body.slice(0, -2).toString()
+            }
+          }
+        })
+        // console.log(lines.toString())
+        resolve(resultObj)
       } else {
         resolve()
       }
