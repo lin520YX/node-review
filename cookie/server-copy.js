b07cb4f20d3787d7aba4fad3d0491cc751478600
lyf <lin13177896353@163.com>
2021-11-07 23:36:45 +0800

    cookie

--- cookie/server-copy.js
+++ cookie/server-copy.js
@@ -3,7 +3,11 @@ const querystring = require('querystring')
 const crypto = require('crypto')
 // 服务端设置的是set-cookie 客户端是cookie字段
 function sign (val) {
-  return crypto.createHmac('sha256', 'haHa').update(val).digest('base64')
+  // 在传输base64 时候需要转化+ = /
+  //  / => _
+  // + => -
+  // = => 空
+  return crypto.createHmac('sha256', 'haHa').update(val + '').digest('base64').replace(/\/|\=|\+/g, '')
   // 加盐算法 放入不同的密钥 产生不同的结果 不可逆
 }
 http.createServer((req, res) => {
@@ -32,7 +36,7 @@ http.createServer((req, res) => {
   }
   req.getCookie = function (key, option = {}) {
     let result = querystring.parse(req.headers.cookie, '; ', '=')
-    let [value, s] = result[key].split('.')
+    let [value, s] = (result[key] || '').split('.')
 
     if (option.signed) {
       if (s == sign(value)) {
@@ -44,29 +48,17 @@ http.createServer((req, res) => {
       return value
     }
   }
-  if (req.url == '/read') {
-    // name=lyf; age=28  
-    // 多个分号空格隔开
-    let res = req.getCookie('name', {
-      signed: true
-    })
-    res.end(JSON.stringify(res))
-  } else if (req.url == '/write') {
-    // cookie 的存活时间 max-age/expires
-    // max-age 以秒为单位
-    // expires  绝对时间
-
-    // domain 针对那个域名生效 二级域名
-    // res.setHeader('Set-Cookie', ['name=lyf; max-age=10; domain', 'age=28'])
-    // res.end('write ok')
-    // path 限制只能在某个路径访问cookie
-    // httpOnly 防止浏览器随意更改
-    res.setCookie('name', 'lyf', {
-      maxAge: 10,
-      httpOnly: 'true'
-    })
-    res.end('write ok')
-
+  //  客户端访问次数
+  if (req.url === '/visit') {
+    let visit = req.getCookie('visit')
+    if (visit) {
+      visit++
+    } else {
+      visit = 1
+    }
+    res.setHeader('Content-Type', 'text/html;charset=utf-8')
+    res.setCookie('visit', visit, { signed: true })
+    res.end(`当前用户第${visit}次访问我`)
   }
 }).listen(9999, () => {
   console.log('server port 9999')
