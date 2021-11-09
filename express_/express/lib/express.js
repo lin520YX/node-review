57d237daf55748405c95a4da526d7463a28ddc3f
lyf <lin13177896353@163.com>
2021-11-09 21:26:02 +0800

    application && router

--- express_/express/lib/express.js
+++ express_/express/lib/express.js
@@ -1,32 +1,5 @@
-const http = require('http')
-const url = require('url')
-
-const routers = [{
-  path: '*', method: 'all', handler: (req, res) => {
-    res.end('Not Found')
-  }
-}]
+const Application = require('./application')
 const createApplication = function () {
-  return {
-    get (path, cb) {
-      routers.push({
-        path, method: 'get', handler: cb
-      })
-    },
-    listen (...args) {
-      const server = http.createServer((req, res) => {
-        let { pathname } = url.parse(req.url)
-        let requestMethod = req.method.toLowerCase()
-        for (i = 0; i < routers.length; i++) {
-          let { path, method, handler } = routers[i]
-          if (pathname == path && method == requestMethod) {
-            return handler(req, res)
-          }
-        }
-        routers[0].handler(req, res)
-      })
-      server.listen(...args)
-    }
-  }
+  return new Application()
 }
 module.exports = createApplication
