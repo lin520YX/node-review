253087d27532d40ef58a88bc0d6e9e5ce17971b7
林云富 <lin13177896353@163.com>
2021-11-13 00:19:14 +0800

    ejs

--- ejs.js
+++ ejs.js
@@ -4,11 +4,26 @@ const path = require('path')
 const fs = require('fs')
 function renderFile(filename,data,cb){
   let template = fs.readFileSync(filename,'utf8')
-  template = template.replace(/<%=(.+?)%>/g,function(){
-    return data[arguments[1]]
+  let head = `let str = ''\r\n`
+  head +=`with(obj){\r\n`
+  head +="str+=`"
+  head +=template.replace(/<%(.+?)%>/g,function(){
+    return '`\r\n'+arguments[1]+'\r\nstr+=`'
   })
-  cb(null,template)
+  let tail = '`\r\n}\r\n return str'
+
+  let fn = new Function('obj',head+tail)
+ cb(null,fn(data))
+  // 1 替换<%%>
+  // 2 把需要的东西拼凑在一个字符串 
+  // 3 new Function
+  // 4 with
+  // template = template.replace(/<%=(.+?)%>/g,function(){
+  //   return data[arguments[1]]
+  // })
+  // cb(null,template)
 }
-renderFile(path.resolve(__dirname,'template.html'),{name:'lyf'},function(err,data){
+renderFile(path.resolve(__dirname,'template.html'),{name:'lyf',arr:[1,2,3]},function(err,data){
   console.log(err,data)
 })
+
